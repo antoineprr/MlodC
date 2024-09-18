d89bbdf6db3045e14a583f79881a1746e5333085
Antoine Poirier <antoine.poirier@etu.imt-nord-europe.fr>
2024-09-18 15:41:24 +0200

    td noté

---
+++ poirier.c
@@ -0,0 +1,45 @@
+// Antoine Poirier
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int *repeat(int t1[], int t2[], int n){
+    int *ptab;
+    int taille = 0;
+    for (int i=0; i<n; i++){
+        taille+=t1[i];
+    }
+    ptab = calloc(taille, sizeof(int));
+    int cpt = 0;
+    for (int j=0; j<n; j++){
+        for (int k=0; k<t1[j]; k++){
+            ptab[cpt]=t2[j];
+            cpt++;
+        }
+    }
+    printf("%d", ptab);
+    return ptab;
+}
+
+void afficherTable(int *ptab, int taille){
+    printf("(");
+    printf(ptab[0]);
+    for (int i=1; i<taille; i++){
+        printf(",");
+        printf("%d", ptab[i]);
+    }
+    printf(")");
+}
+
+
+int main(void) {
+    int t1[3] = {1,2,4};
+    int t2[3] = {10,3,8};
+    int n = sizeof(t1)/sizeof(int);
+    int *ptab = repeat(t1,t2,n);
+    //afficherTable(ptab, n);
+    free(ptab);
+    return 0;
+}
