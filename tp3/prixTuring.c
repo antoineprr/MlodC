558ac4b0b1f95ab65998a7a94dcddefe111ec739
Antoine Poirier <antoine.poirier@etu.imt-nord-europe.fr>
2024-09-20 10:05:01 +0200

    fonction nbrWinners+debut readWinners

--- tp3/prixTuring.c
+++ tp3/prixTuring.c
@@ -16,11 +16,60 @@
 #include <errno.h>
 #include <assert.h>
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// DONNEES NECESSAIRES
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 typedef struct {
-	int annee;
-	char nom[64];
-	char nature[252];
+	unsigned short annee;
+	char *nom;  // \0 at the end MANDATORY
+	char *nature;
 } Gagnant ;
+
+
+short getYearFromBuffer(char buffer[]){
+	short year;
+	sscanf(buffer, "%4hd", &year);
+	return year;
+}
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// FONCTIONS DEMANDEES
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+int numberOfWinners(FILE *f){
+	int nbr = 1;
+	char nextChar;
+	fseek(f, 0, 0);
+	while(!feof(f)){
+		nextChar = getc(f);
+		if (nextChar=='\n'){
+			nbr++;
+		}
+	}
+	return nbr;
+}
+
+
+Gagnant **readWinners(FILE *f){
+	Gagnant **p;
+	int n = numberOfWinners(f);
+	p = calloc(n, sizeof(Gagnant *));
+	fseek(f, 0, 0);
+	for (int i=0; i<n; i++){
+		Gagnant *w;
+		w = malloc(sizeof(*w));
+		char buffer[1024];
+		fgets(buffer, sizeof(buffer)/sizeof(char),f);
+		w->annee = getYearFromBuffer(buffer);
+		printf("%d\n",w->annee);
+		int cpt = 0;
+	}
+
+
+	return p;
+}
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -53,22 +102,30 @@ int main(int argc, char** argv)
 
 	fOut = fopen(outputFilename, "w");
 
-	char buffer[5];
-	char test ; 
-	int cpt;
+	int nbr = numberOfWinners(fIn);
+	Gagnant **p = readWinners(fIn);
 
-	for (int i=0; i<4; i++){
-		fgets(buffer, 5, fIn);
-		test = fgetc(fIn);
-		cpt=0;
-		while((test!="\n")&(cpt<100)){
-			test = fgetc(fIn);
-			printf("%d\n",cpt);
-			printf("%c\n", test);
-			cpt++;
-		}
-		fwrite(&buffer, sizeof(buffer), 1, fOut);
+	for (int i=0; i<nbr; i++){
+		free(p[i]);
 	}
+	free(p);
+
+
+	// char buffer[5];
+	// char test ; 
+	// int cpt;
+
+	// for (int i=0; i<2; i++){
+	// 	fgets(buffer, 5, fIn);
+	// 	cpt=0;
+	// 	while((test!='\n')&(cpt<150)){
+	// 		test = fgetc(fIn);	
+	// 		printf("%d : ",cpt);
+	// 		printf("%c\n", test);
+	// 		cpt++;
+	// 	}
+	// 	fwrite(&buffer, sizeof(buffer), 1, fOut);
+	// }
 
 	fclose(fIn);
 	fclose(fOut);
